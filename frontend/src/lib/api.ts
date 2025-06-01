82e7d30234c1a7d0b5607d5ef0f099b4c0a3093f
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-01 18:49:28 +0200

    Fix CORS errors

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import { hc } from "hono/client";
 import type { ApiRoutes } from "@server/app";
 
-const client = hc<ApiRoutes>("http://localhost:5173");
+const client = hc<ApiRoutes>("/");
 
 export const api = client.api;
