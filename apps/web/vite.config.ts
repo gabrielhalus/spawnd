4c9d7d149280552d9ad2862e63ccbfd548af7c1c
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-08 18:08:17 +0200

    [web] core: setup TanStack Router

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,20 +1,19 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-
-import tailwindcss from '@tailwindcss/vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import react from '@vitejs/plugin-react'
-
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
-    tailwindcss()],
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
