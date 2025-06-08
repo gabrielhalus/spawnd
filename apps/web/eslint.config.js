61a2071918e52d96597ed2e712bc60466d4f2cc3
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-08 17:10:59 +0200

    [web] refactor: add web app

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,18 @@
+import createConfig from "@spawnd/eslint-config/create-config";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+
+export default createConfig({
+  react: true,
+}, {
+  plugins: {
+    "@tanstack/query": pluginQuery,
+  },
+  rules: {
+    "antfu/top-level-function": "off",
+    "@tanstack/query/exhaustive-deps": "error",
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md", "~__root.tsx"],
+    }],
+  },
+});
