2a69e63d40175f528cf6b80cdcc6a6a671fd03a5
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-08 23:57:41 +0200

    [shared] add `users` schemas

---
+++ packages/shared/src/schemas/users.ts
@@ -0,0 +1,39 @@
+import type { SQL } from "drizzle-orm";
+import type { AnySQLiteColumn } from "drizzle-orm/sqlite-core";
+
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { nanoid } from "nanoid";
+import { z } from "zod";
+
+export const users = sqliteTable("users", {
+  id: text("id").primaryKey().$defaultFn(() => nanoid()),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  createdAt: integer("created_at").notNull().$defaultFn(() => new Date().getTime()),
+  updatedAt: integer("updated_at").notNull().$defaultFn(() => new Date().getTime()),
+});
+
+export const selectUserSchema = createSelectSchema(users);
+
+export const insertUserSchema = createInsertSchema(users, {
+  name: z.string().min(1, { message: "Name is required" }).min(3, { message: "Name must be at least 3 characters long" }).max(20, { message: "Name must be less than 20 characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(1, { message: "Password is required" }).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+    message: "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+  }),
+});
+
+export const createUserSchema = insertUserSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const updateUserSchema = insertUserSchema.partial();
+
+export function lower(email: AnySQLiteColumn): SQL {
+  return sql`(lower(${email}))`;
+}
