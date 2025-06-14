918158e8d6f3ce994fc4b50624e1b3c09a510c46
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-14 11:10:16 +0200

    [api] serve static files from the React Build

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import { serveStatic } from "hono/bun";
 
 import auth from "@/routes/auth";
 import users from "@/routes/users";
@@ -7,10 +8,16 @@ import serveEmojiFavicon from "./middlewares/serve-emoji-favicon";
 
 const app = new Hono({ strict: false });
 
+// Middleware
 app.use(logger(), serveEmojiFavicon("🔥"));
 
+// API routes
 app.basePath("/api")
   .route("/auth", auth)
   .route("/users", users);
+
+// Serve static files from the React Build
+app.use("/*", serveStatic({ root: "../web/dist" }))
+app.use("*", serveStatic({ root: "../web/dist", path: "index.html" }));
 
 export default app;
