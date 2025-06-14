a9ed668f01f35ff3d54dae01ab18b25e05ab254b
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-14 15:17:48 +0200

    feat(api): add `deleteUser` to database queries

--- apps/api/src/db/queries/users.ts
+++ apps/api/src/db/queries/users.ts
@@ -6,36 +6,50 @@ import { db } from "@/db";
 import { lower, users } from "@spawnd/shared/schemas/users";
 
 /**
- * Get all users
- * @returns All users
+ * Get all users.
+ * 
+ * @returns All users.
  */
 export async function getAllUsers() {
   return db.select().from(users);
 }
 
 /**
- * Get a user by their ID
- * @param id - The ID of the user
- * @returns The user
+ * Get a user by its ID.
+ * 
+ * @param id - The ID to look up.
+ * @returns The matching user.
  */
 export async function getUserById(id: string) {
   return db.select().from(users).where(eq(users.id, id)).get();
 }
 
 /**
- * Get a user by their email
- * @param email - The email of the user
- * @returns The user
+ * Get a user by its email.
+ * 
+ * @param email - The email to look up.
+ * @returns The matching user.
  */
 export async function getUserByEmail(email: string) {
   return db.select().from(users).where(eq(lower(users.email), email.toLowerCase())).get();
 }
 
 /**
- * Insert a user
- * @param user - The user to insert
- * @returns The inserted user
+ * Insert a new user.
+ * 
+ * @param user - The user data to insert.
+ * @returns The inserted user.
  */
 export async function insertUser(user: typeof insertUserSchema._type) {
   return db.insert(users).values(user).returning().get();
+}
+
+/**
+ * Delete a user by its ID.
+ * 
+ * @param id - The ID of the user to delete.
+ * @returns The deleted user.
+ */
+export async function deleteUser(id: string) {
+  return db.delete(users).where(eq(users.id, id)).returning().get();
 }
