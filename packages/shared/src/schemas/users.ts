80ccfdd1ea6c440cb81a051b2e0c4d25d6e30924
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-14 14:21:23 +0200

    feat(shared): add `avatar` to users model

--- packages/shared/src/schemas/users.ts
+++ packages/shared/src/schemas/users.ts
@@ -12,6 +12,7 @@ export const users = sqliteTable("users", {
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   password: text("password").notNull(),
+  avatar: text("avatar"),
   createdAt: integer("created_at").notNull().$defaultFn(() => new Date().getTime()),
   updatedAt: integer("updated_at").notNull().$defaultFn(() => new Date().getTime()),
 });
