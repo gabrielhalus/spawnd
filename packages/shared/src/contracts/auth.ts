20c0d5b2d89441dccbcf88b8190d3d66e819a6f6
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-15 11:19:21 +0200

    feat(shared): add contracts for register validation

--- packages/shared/src/contracts/auth.ts
+++ packages/shared/src/contracts/auth.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
+import { createUserSchema } from "../schemas/users";
 
+// Login
 export const loginInputSchema = z.object({
   email: z.string().min(1, "Email is required").email("Invalid email"),
   password: z.string().min(1, "Password is required"),
@@ -18,5 +20,22 @@ const loginErrorSchema = z.object({
 export const loginOutputSchema = z.union([loginSuccessSchema, loginErrorSchema]);
 
 export type LoginInput = z.infer<typeof loginInputSchema>;
-
 export type LoginOutput = z.infer<typeof loginOutputSchema>;
+
+// Register
+export const registerInputSchema = createUserSchema;
+
+const registerSuccessSchema = z.object({
+  success: z.literal(true),
+  accessToken: z.string(),
+});
+
+const registerErrorSchema = z.object({
+  success: z.literal(false),
+  error: z.string(),
+});
+
+export const registerOutputSchema = z.union([registerSuccessSchema, registerErrorSchema]);
+
+export type RegisterInput = z.infer<typeof registerInputSchema>;
+export type RegisterOutput = z.infer<typeof registerOutputSchema>;
