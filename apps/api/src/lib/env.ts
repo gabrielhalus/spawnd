e4656252ec3b9b3626acbc676c69fdbde186f6e8
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-23 17:43:24 +0200

    refactor(api): update env file resolution

--- apps/api/src/lib/env.ts
+++ apps/api/src/lib/env.ts
@@ -1,9 +1,10 @@
+/* eslint-disable node/no-process-env */
+
 import dotenv from "dotenv";
 import path from "node:path";
 import { z } from "zod";
 
-// eslint-disable-next-line node/no-process-env
-dotenv.config({ path: process.env.NODE_ENV === "test" ? path.resolve(process.cwd(), ".env.test") : undefined });
+dotenv.config({ path: process.env.NODE_ENV === "test" ? path.resolve(".env.test") : undefined });
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["production", "development", "test"]),
@@ -17,7 +18,6 @@ export type Env = z.infer<typeof envSchema>;
 let env: Env;
 
 try {
-  // eslint-disable-next-line node/no-process-env
   env = envSchema.parse(process.env);
 } catch (e) {
   const error = e as z.ZodError;
