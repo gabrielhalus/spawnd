a29fb947e98d413bcd398fcc3cf94f748a27fedf
Gabriel Halus <gabrielhalus@gmail.com>
2025-06-23 19:21:21 +0200

    fix(web): missing vite alias

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@spawnd/shared": path.resolve(__dirname, "../../packages/shared/src"),
     },
   },
   server: {
